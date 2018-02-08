1eb8334861adbb1dea02624abe001343e0a84374
gaiyulong <gyl@acewill.cn>
2018-02-08 18:29:04 +0800

    initing...

---
+++ components/vslider/vslider.js
@@ -0,0 +1,41 @@
+Component({
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    innerText: {
+      type: String,
+      value: 'default value',
+    }
+  },
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
+  },
+  methods: {
+    // 这里是一个自定义方法
+    customMethod: function () { },
+    changeIndicatorDots: function (e) {
+      this.setData({
+        indicatorDots: !this.data.indicatorDots
+      })
+    },
+    changeAutoplay: function (e) {
+      this.setData({
+        autoplay: !this.data.autoplay
+      })
+    },
+    intervalChange: function (e) {
+      this.setData({
+        interval: e.detail.value
+      })
+    },
+    durationChange: function (e) {
+      this.setData({
+        duration: e.detail.value
+      })
+    }
+  }
+})
